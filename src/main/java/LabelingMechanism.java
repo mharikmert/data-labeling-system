ca5c981c6ec1c8a3369d96913daa0558790450aa
Yunus Emre <yeertunc63@gmail.com>
2020-12-01 22:32:21 +0300

    Abstract LabelingMechanism Class is added

---
+++ src/main/java/LabelingMechanism.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public abstract class LabelingMechanism {
+
+    public abstract void labelingMechanism(User user , Instance instance , ArrayList<Label> labels , long maxNumberOfLabels);
+
+}
