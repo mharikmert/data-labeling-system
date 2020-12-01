f1b97bc02a3b0be38719851689ac0cff0b16a6cf
Yunus Emre <yeertunc63@gmail.com>
2020-12-01 22:33:33 +0300

    RandomMechanism Class is added

---
+++ src/main/java/RandomMechanism.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import org.apache.log4j.Logger;
+
+public class RandomMechanism extends LabelingMechanism{
+
+    @Override
+    public void labelingMechanism(User user, Instance instance, ArrayList<Label> labels , long maxNumberOfLabels) {
+
+    }
+
+}
