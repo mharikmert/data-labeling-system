55f90347017cfcbb60b4dda89324e2c23e9b07ed
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-02 11:31:36 +0300

    dataset and users objects created in main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
+//import necessary libraries
+import java.util.ArrayList;
+
+/*
+    Main class
+ */
 public class Main {
-
+    /*
+        Main method
+    */
     public static void main(String[] args) {
+        //create a json file reader
+        JsonFileReader jsonFileReader = new JsonFileReader();
 
-        System.out.println("Hello world");
+        //create dataset object for json files with readDatasetFile method
+        Dataset dataset1 = jsonFileReader.readDatasetFile("input_dataset1.json");
+        Dataset dataset2 = jsonFileReader.readDatasetFile("input_dataset2.json");
+
+        //create a users arrayList to hold inside users in json file
+        ArrayList<User> users = jsonFileReader.readUserFile("input_users.json");
 
     }
 
