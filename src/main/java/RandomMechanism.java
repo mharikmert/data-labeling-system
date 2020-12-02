61486387bfd5b7b67d6fa2d9dd0bcd32ef32ae27
Muhammed Enes Aktürk <enesakturk999@hotmail.com>
2020-12-02 13:31:36 +0300

    RandomMechanism class and comments added

--- src/main/java/RandomMechanism.java
+++ src/main/java/RandomMechanism.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import org.apache.log4j.Logger;
 
 public class RandomMechanism extends LabelingMechanism{
 
@@ -10,6 +9,39 @@ public class RandomMechanism extends LabelingMechanism{
 
     @Override
     public void labelingMechanism(User user, Instance instance, ArrayList<Label> labels , long maxNumberOfLabels) {
+
+        // we create random number for number of labels for an instance
+        int numberOfLabels = (int)(Math.random()*(maxNumberOfLabels-1))+1;
+
+        // in this for loop , we select the labels
+        for (int i=0 ; i<numberOfLabels ; i++){
+            // select random label with createRandomLabel method
+            Label randomLabel = createRandomLabel(labels);
+            // if the instance has no label , add the label directly , we don't need to control.
+            if (instance.getLabels().size() == 0){
+                instance.addLabelToInstance(randomLabel);
+            }
+            else{
+                // in this for loop , we look all labels inside the instance ,
+                for (int j=0 ; j<instance.getLabels().size() ; j++){
+                    // if a label is added before , create new label for
+                    // this instance , don't add same label again
+                    while (instance.getLabels().get(j).getId() == randomLabel.getId()){
+                        // create new label
+                        randomLabel = createRandomLabel(labels) ;
+                    }
+                }
+                // add the label to instance
+                instance.addLabelToInstance(randomLabel);
+            }
+
+        }
+        user.addInstanceToUser(instance);
+    }
+
+    Label createRandomLabel(ArrayList<Label> labels){
+        // randomly select one label from labels array
+        return labels.get((int)(Math.random()*labels.size()));
 
     }
 
