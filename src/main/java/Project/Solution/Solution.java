e154aad4e071cb78426999d9d3b4fd4fdee3d290
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-05 00:13:50 +0300

    implementing singleton design pattern in Solution class

--- src/main/java/Project/Solution/Solution.java
+++ src/main/java/Project/Solution/Solution.java
@@ -7,25 +7,35 @@ import Project.Problem.SelectedProblem;
 
 import java.util.ArrayList;
 
-    public class Solution {
-    //Solutions will be in there
-    public void solveProblem(Dataset dataset, ArrayList<User> users){
-        Problem problem;
-        // in this switch case statement , solutions differ for datasets , but for first itearation , we have just random labeling and we
-        // can not create different problem types , we just create selectedproblem() object.
-        switch(dataset.getDatasetName()){
-            case "Sentiment Dataset":
-                problem = new SelectedProblem();
-                problem.runMechanism(users,dataset);
-                break;
-            case "Multi-label Topic Classification Dataset":
-                problem = new SelectedProblem();
-                problem.runMechanism(users,dataset);
-                break;
-            default:
-                problem = null;
-                System.out.println("Undetermined problem type !");
-        }
+    /*
+    * Implementing Solution class with singleton design pattern
+    * */
 
-    }
+    public class Solution {
+
+        // this class can not be instantiated
+        private Solution(){}
+
+        //create an object of Solution
+        private static final Solution solution = new Solution();
+
+        //get the only available object
+        public static Solution getSolution(){
+            return solution;
+        }
+        //Solutions will be in there
+        public void solveProblem(Dataset dataset, ArrayList<User> users){
+            Problem problem;
+            // in this switch case statement , solutions differ for datasets , but for first iteration , we have just random labeling and we
+            // can not create different problem types , we just create selectedProblem() object.
+            switch(dataset.getDatasetName()){
+                case "Sentiment Dataset":
+                case "Multi-label Topic Classification Dataset":
+                    problem = new SelectedProblem();
+                    problem.runMechanism(users,dataset);
+                    break;
+                default:
+                    System.out.println("Undetermined problem type !");
+            }
+        }
 }
