df17884c024c69ee7ba323c634c9e43c7864c076
Muhammed Enes Aktürk <enesakturk999@hotmail.com>
2020-12-15 23:36:53 +0300

    config.json changed,JsonReader updated

--- src/main/java/Project/JsonIO/JsonFileReader.java
+++ src/main/java/Project/JsonIO/JsonFileReader.java
@@ -8,6 +8,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,6 +22,7 @@ public class JsonFileReader {
     private final ArrayList<Label> labels;
     private final ArrayList<Instance> instances;
     private final ArrayList<User> users;
+    private long numberofuser ;
 
     //class constructor
     public JsonFileReader() {
@@ -27,6 +30,41 @@ public class JsonFileReader {
         labels = new ArrayList<>();
         instances = new ArrayList<>();
         users = new ArrayList<>();
+    }
+
+
+    public String getDatasetPath(String path){
+
+        String datasetPath = "";
+
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader fileReader = new FileReader(path)) {
+
+            //create an object from jasonParser parse method
+            Object obj = jsonParser.parse(fileReader);
+            //create a jsonObject from obj
+            JSONObject jsonObject = (JSONObject) obj;
+            //create a json array from json object to hold user info in it
+
+            long currentDatasetId = (long) jsonObject.get("currentDatasetID");
+
+            JSONArray datasetsJSON = (JSONArray)jsonObject.get("datasets");
+
+            for (Object o : datasetsJSON) {
+                //create a current user in the same manner
+                numberofuser = (long) ((JSONObject) o).get("number of user");
+                if((long) ((JSONObject) o).get("dataset id") == currentDatasetId){
+                    return (String) ((JSONObject) o).get("path");
+                }
+            }
+
+        }catch (IOException | ParseException e){
+            e.printStackTrace();
+        }
+
+        return "Path not found" ;
+
     }
 
     //readDataSet method to read dataset type json files with String path parameter
@@ -71,7 +109,7 @@ public class JsonFileReader {
                 instances.add(currentInstance);
             }
             //return a dataset object with attributes in json file
-            return new Dataset(id, datasetName,maxNumberOfLabelsPerInstance, labels, instances);
+            return new Dataset(id, datasetName,maxNumberOfLabelsPerInstance, labels, instances, numberofuser);
 
         //catch io or parse exception
         } catch (IOException | ParseException e) {
@@ -103,7 +141,8 @@ public class JsonFileReader {
             for (Object o : usersJSON) {
                 //create a current user in the same manner
                 User currentUser = new User((long) ((JSONObject) o).get("userID"),
-                        (String) ((JSONObject) o).get("userName"), (String) ((JSONObject) o).get("userType"));
+                        (String) ((JSONObject) o).get("userName"), (String) ((JSONObject) o).get("userType"),
+                        (double) ((JSONObject) o).get("ConsistencyCheckProbability"));
                 //adding users to the users
                 users.add(currentUser);
                 logger.info("userManager: created " + currentUser.getUserName() + " as " + currentUser.getUserType());
