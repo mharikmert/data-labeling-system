43a09ff2a85a9c2d403fa07cf1c6b332377644cb
kboyaci <74654666+kboyaci@users.noreply.github.com>
2020-12-21 00:55:15 +0300

    Update Main.java

--- src/main/java/Project/Main.java
+++ src/main/java/Project/Main.java
@@ -8,28 +8,19 @@ import Project.Solution.Solution;
 import java.io.File;
 import java.util.ArrayList;
 
+import org.json.JSONWriter;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        //create a json file reader
         ArrayList<User> users = new ArrayList<>();
         ArrayList<Dataset> datasets =new ArrayList<>();
+         //create a json file reader
         JsonFileReader jsonFileReader = new JsonFileReader("config.json",datasets,users);
 
-        //create dataset object for json files with readDatasetFile method
-        for (Dataset d:datasets) {
-            System.out.println(d.toString());
-        }
-        System.out.println("*********************\n");
-        for (User d:users) {
-            System.out.println(d.toString());
-        }
-
         //get solution with solve problem method
-        Solution.getSolution().solveProblem(datasets.get(1), users,datasets);
-
-
+        Solution.getSolution().solveProblem(users,datasets);
     }
 
 }
