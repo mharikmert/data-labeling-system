4d48e5b9fbb67d2e8b7371103b06676e5cb5f42d
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-24 15:45:14 +0300

    removing unused

--- src/test/java/Labeling/RandomMechanismTest.java
+++ src/test/java/Labeling/RandomMechanismTest.java
@@ -27,47 +27,9 @@ public class RandomMechanismTest {
          * After running this method , createRandomLabel method should return not null value
          *
          **/
-
         for (Dataset dataset:
              datasets) {
             Assert.assertNotNull(randomMechanism.createRandomLabel(dataset.getLabels()));
         }
-
-
-
     }
-
-/*
-    @Test
-    public void testLabelingMechanism(){
-
-        /*
-         *   After this method , the parameter instance must be added into user's 'instances' and
-         *   one of the label in labels array (in this test we just have 1 label in labels array) ,
-         *   must be added into instance's labels array.
-         *
-
-        User user = new User(1,"test","test user");
-        Instance instance = new Instance(1,"test instance");
-        ArrayList<Label> labels = new ArrayList<>();
-        labels.add(new Label(1,"test label"));
-
-        randomMechanism.labelingMechanism(user,instance,labels,1);
-
-        Assert.assertNotNull(user.getInstances().get(0).getLabels().get(0));
-
-    }
-
-    @Test
-    public void testCreateRandomLabel(){
-
-        /*
-         * After running this method , createRandomLabel method should return not null value
-         *
-         *
-
-        Assert.assertNotNull(randomMechanism.createRandomLabel(dataset.getLabels()));
-
-    }
-*/
 }
