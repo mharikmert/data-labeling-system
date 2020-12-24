2c3bbdc2291731eeff259b5ae8d9baeb11169d5c
Yunus Emre <yeertunc63@gmail.com>
2020-12-24 11:05:05 +0300

    JsonFileReader Test class is updated.

--- src/test/java/JsonIO/JsonFileReaderTest.java
+++ src/test/java/JsonIO/JsonFileReaderTest.java
@@ -2,6 +2,7 @@ package JsonIO;
 
 import Project.Dataset;
 import Project.JsonIO.JsonFileReader;
+import Project.User;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
@@ -12,6 +13,21 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 public class JsonFileReaderTest {
+
+    ArrayList<User> users = new ArrayList<>();
+    ArrayList<Dataset> datasets =new ArrayList<>();
+    //create a json file reader
+
+    @Test
+    public void testJsonFileReaderConstructor(){
+        JsonFileReader jsonFileReader = new JsonFileReader("config.json",datasets,users);
+
+        Assert.assertTrue(users.size() > 0 && datasets.size() > 0);
+        
+    }
+
+
+
 /*
     @Test
     public void testReadDataset(){
