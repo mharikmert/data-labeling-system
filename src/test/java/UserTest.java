f906980b8c210385e999cc9e2f1c8ff3a3add40e
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-24 15:46:31 +0300

    user test is added

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,17 @@
+import Project.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTest {
+    private long userID;
+    private String userName;
+    private String userType;
+    private double consistenctCheckProbability;
+
+    //constructor test with check arraylist datasets
+    @Test
+    public void constructorTest(){
+        User user = new User(userID,userName, userType, consistenctCheckProbability);
+        Assert.assertNotNull(user.getDatasets());
+    }
+}
