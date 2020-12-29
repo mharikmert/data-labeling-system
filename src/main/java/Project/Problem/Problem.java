b78bab12880f8f99114ef9fe2b7623c26583a1ff
kboyaci <74654666+kboyaci@users.noreply.github.com>
2020-12-30 00:46:57 +0300

    Update Problem.java

--- src/main/java/Project/Problem/Problem.java
+++ src/main/java/Project/Problem/Problem.java
@@ -7,14 +7,8 @@ import Project.User;
 import java.util.ArrayList;
 
 public abstract class Problem {
-    /*
-        This class will be the most parent class of all kind of problems
-     */
 
     LabelingMechanism labelingMechanism ;
-    public abstract void run(ArrayList<User> users, Dataset dataset,ArrayList<Dataset> datasets);
-    public abstract void runBot(ArrayList<User> users, Dataset dataset,ArrayList<Dataset> datasets);
-    public abstract void runUser(User user, ArrayList<User> users, Dataset dataset,ArrayList<Dataset> datasets);
-
+    public abstract void runMechanism(ArrayList<User> users, Dataset dataset,ArrayList<Dataset> datasets);
 
 }
