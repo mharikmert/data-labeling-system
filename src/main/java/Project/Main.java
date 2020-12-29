8666f76b7d35af36dad567e59ec42309ffaee137
Muhammed Enes Aktürk <enesakturk999@hotmail.com>
2020-12-29 19:16:00 +0300

    infinite loop is added

--- src/main/java/Project/Main.java
+++ src/main/java/Project/Main.java
@@ -7,13 +7,15 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<User> users = new ArrayList<>();
-        ArrayList<Dataset> datasets =new ArrayList<>();
-         //create a json file reader
-        JsonFileReader jsonFileReader = new JsonFileReader("config.json",datasets,users);
+        while(true){
+            ArrayList<User> users = new ArrayList<>();
+            ArrayList<Dataset> datasets =new ArrayList<>();
+            //create a json file reader
+            JsonFileReader jsonFileReader = new JsonFileReader("config.json",datasets,users);
 
-        //get solution with solve problem method
-        Solution.getSolution().solveProblem(users,datasets);
+            //get solution with solve problem method
+            Solution.getSolution().solveProblem(users,datasets);
+        }
     }
 
 }
