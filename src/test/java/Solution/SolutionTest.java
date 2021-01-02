847b79f05b433ae2aed19b90ffc3c0a5816ee888
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2021-01-02 15:28:09 +0300

    SolutionTest class

--- src/test/java/Solution/SolutionTest.java
+++ src/test/java/Solution/SolutionTest.java
@@ -1,8 +1,12 @@
 package Solution;
 
 import Project.Solution.Solution;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SolutionTest {
-
+    @Test
+    public void testGetSolution(){
+        Assert.assertNotNull(Solution.getSolution());
+    }
 }
