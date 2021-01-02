ec9cf7ebc8a5fcb8eccf85bd02e917d4e1bd2d40
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2021-01-02 15:25:09 +0300

    UserIn some corrections and protected class test for UserMetrics class

--- src/test/java/Metrics/UserMetricsTest.java
+++ src/test/java/Metrics/UserMetricsTest.java
@@ -10,39 +10,34 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class UserMetricsTest {
     private ArrayList<User> users = new ArrayList<>();
     private ArrayList<Dataset> datasets = new ArrayList<>();
-    JsonFileReader jsonFileReader = new JsonFileReader("config.json", datasets,users);
 
     @BeforeEach
     public void init(){
+        new JsonFileReader("config.json", datasets,users);
         Solution.getSolution().solveProblem(users, datasets);
     }
 
     @Test
     public void testNumberOfUsersAssigned(){
         for(User user: users){
-            Assert.assertNotNull(UserMetrics.getUserMetrics().numberOfDatasetsAssigned(user));
+            Assert.assertTrue(UserMetrics.getUserMetrics().numberOfDatasetsAssigned(user) >=0);
         }
     }
-
-    // @Test
-    // public void testFrequencyListOfInstances(){
-    //     for(User user: users){
-    //         for(Dataset dataset: datasets)
-    //             Assert.assertNotNull(UserMetrics.getUserMetrics().frequencyListOfInstances(dataset,user));
-    //     }
-    // }
 
     @Test
     public void testNumberOfInstancesLabeled(){
         for(User user: users){
-            Assert.assertNotNull(UserMetrics.getUserMetrics().numberOfInstancesLabeled(user));
+            Assert.assertTrue(UserMetrics.getUserMetrics().numberOfInstancesLabeled(user)>=0);
         }
     }
 
@@ -55,21 +50,21 @@ public class UserMetricsTest {
     @Test
     public void testNumberOfUniqueInstancesLabeled(){
         for(User user: users){
-            Assert.assertNotNull(UserMetrics.getUserMetrics().numberOfUniqueInstancesLabeled(user));
+            Assert.assertFalse(UserMetrics.getUserMetrics().numberOfUniqueInstancesLabeled(user)<0);
         }
     }
 
     @Test
     public void testAverageTimeSpentInLabeling(){
         for(User user: users){
-            Assert.assertTrue(UserMetrics.getUserMetrics().averageTimeSpentInLabeling(user) != 0);
+            Assert.assertFalse(UserMetrics.getUserMetrics().averageTimeSpentInLabeling(user)<0);
         }
     }
 
     @Test
     public void testStdDeviationOfTimeSpentInLabeling(){
         for(User user: users){
-            Assert.assertNotNull(UserMetrics.getUserMetrics().stdDeviationOfTimeSpentInLabeling(user));
+            Assert.assertFalse(UserMetrics.getUserMetrics().stdDeviationOfTimeSpentInLabeling(user)<0);
         }
     }
 
@@ -77,14 +72,25 @@ public class UserMetricsTest {
     public void testConsistency(){
         for(Dataset dataset: datasets)
             for(User user: users)
-                Assert.assertNotNull(UserMetrics.getUserMetrics().consistency(dataset, user));
+                Assert.assertFalse( UserMetrics.getUserMetrics().consistency(dataset, user)<0);
     }
 
     @Test
     public void testConsistency2(){
         for(User user: users){
-            Assert.assertNotNull(UserMetrics.getUserMetrics().consistency(datasets, user));
+            Assert.assertEquals(0, UserMetrics.getUserMetrics().consistency(datasets, user), 0);
         }
     }
 
+    @Test //test for protected frequencyListOfInstances method
+    public void testFrequencyListOfInstances() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = UserMetrics.class.getDeclaredMethod("frequencyListOfInstances", Dataset.class, User.class);
+        method.setAccessible(true);
+        UserMetrics userMetrics = UserMetrics.getUserMetrics();
+        for(Dataset dataset: datasets){
+            for(User user:users){
+                Assert.assertNotNull(method.invoke(userMetrics,dataset,user));
+            }
+        }
+    }
 }
